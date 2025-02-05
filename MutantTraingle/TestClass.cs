8837f4cd2e2f0e1be226dad48aba0533b73cbf77
Kuljeet0007 <Kuljeetsingh0977@conestogac.on.ca>
2025-02-05 16:32:47 -0500

    Add project files.

---
+++ MutantTraingle/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+namespace Traingle1
+{
+    public class TestClass
+    {
+        //KULJEETSINGH
+        [Test]
+        public void TestTraingle()
+        {
+            float A = 50, B = 50, C = 90;
+            int flag = 1;
+            int result= Class1.TriangleIsosceles(A, B, C);
+            ClassicAssert.AreEqual(flag, result);
+        }
+    }
+}
---
+++ Traingle1/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace Traingle1
+{
+    public class Class1
+    {
+        public static int TriangleIsosceles(float A,float B, float C )
+
+        {
+            if (A == B || A == C || B == C)
+            {
+                return 1;
+            }
+            else { return 0; }
+        }
+    }
+}
